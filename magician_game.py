54634c988e57361a345b6c9862af715b51757761
MKwanda <mbrkkwanda@gmail.com>
2021-12-26 00:47:29 +0000

    first game code

---
+++ magician_game.py
@@ -0,0 +1,18 @@
+is_magician = False
+is_expert = False
+
+
+if is_magician and is_expert:
+    print('you are a master magician')
+elif is_magician and not is_expert :
+    print('at least you are getting there')
+elif not is_magician and not is_expert:
+    print('You are zuuku')
+
+elif not is_magician  :
+    print('You need magical powers')
+
+
+
+# else:
+#     print('you are Zuuku')
